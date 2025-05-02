1856587566733e6a87e5e5ac34a9d0981f5432ec
Matt Faherty <matt.faherty@gmail.com>
2025-05-02 10:33:53 -0400

    Major Refactor

--- src/dashboard/mod.rs
+++
@@ -1,3 +0,0 @@
-mod compute_dashboard;
-
-pub use compute_dashboard::ComputeDashboard; 
--- src/restorative/mod.rs
+++
@@ -1,5 +0,0 @@
-mod restorative_flow;
-mod educational_module;
-
-pub use restorative_flow::RestorativeFlow;
-pub use educational_module::EducationalModule; 
