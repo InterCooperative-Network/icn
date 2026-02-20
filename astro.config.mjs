18bd7d7342331f6d0eac2de67921d543064c7e41
Matt Faherty <matt.faherty@gmail.com>
2026-02-20 14:43:15 +0000

    Squashed 'website/' content from commit 40fc4c32
    git-subtree-dir: website
    git-subtree-split: 40fc4c32c11e24439ccd6ccade9f15ef7fbec7f6

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'static',
+  integrations: [sitemap()],
+  site: 'https://intercooperative.network',
+  base: '',
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+    },
+  },
+});
